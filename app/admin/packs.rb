d7b412c5b800a76cf6b6cf4cf5e58c16291ebce5
igoo-Y <yang_ig@naver.com>
2021-05-14 21:06:48 +0900

    admin custom, locale, time_zone

--- app/admin/packs.rb
+++ app/admin/packs.rb
@@ -14,5 +14,54 @@ ActiveAdmin.register Pack do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
+  #index 커스텀
+  index do
+    selectable_column
+
+    id_column
+
+    column :image do |pack|
+      if pack.image.attached?
+        image_tag url_for(pack.image), class: 'small_img'
+      else
+        '이미지 없음'
+      end
+    end
+
+    column :product_name
+    column :company_name
+    column :desc
+
+    actions
+  end
+
+  #new, edit 커스텀
+  form do |f|
+    f.inputs do
+      f.input :image, as: :file #active_admin 에서는 f.input :컬럼명, as: :file(원하는 자료 타입) 으로 변경가능.
+      f.input :product_name
+      f.input :company_name
+      f.input :desc
+    end
+    f.actions
+  end
+
+  #show 커스텀
+  show do
+    attributes_table do
+      row :id
+      row :image do |pack|
+        if pack.image.attached?
+          image_tag url_for(pack.image), class: 'small_img'
+        else
+          '이미지 없음'
+        end
+      end
+      row :product_name
+      row :company_name
+      row :desc
+    end
+  end
+
 end
