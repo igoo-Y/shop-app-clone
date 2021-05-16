15c97468884a28479f0fd31c316e4d5d4602587a
igoo-Y <yang_ig@naver.com>
2021-05-16 15:19:08 +0900

    order(address API), model method

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -26,17 +26,7 @@ class CartsController < ApplicationController
       quantity: params[:quantity]
     )
 
-    sample_carts = current_user.carts
-
-    remain_cart = sample_carts.find_by(pack_id: params[:pack_id])
-
-    if remain_cart.present?
-      sum_quantity = remain_cart.quantity + params[:quantity].to_i
-
-      remain_cart.update(quantity: sum_quantity)
-    else
-      cart.save
-    end
+    cart.verified_save
 
     flash[:notice] = "장바구니에 상품이 담겼습니다. 장바구니로 이동하시겠습니까?"
 
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,19 @@
 class Cart < ApplicationRecord
   belongs_to :user
   belongs_to :pack
+
+  def verified_save
+    # cart = self
+    sample_carts = user.carts
+
+    remain_cart = sample_carts.find_by(pack_id: pack_id)
+
+    if remain_cart.present?
+      sum_quantity = remain_cart.quantity + quantity.to_i
+
+      remain_cart.update(quantity: sum_quantity)
+    else
+      save
+    end
+  end
 end
