903fbc8e6f9e662131588327eee398288a3822a2
igoo-Y <yang_ig@naver.com>
2021-05-16 16:36:43 +0900

    custom view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
+  def item_to_currency(item)
+    number_to_currency(item.quantity * item.pack.price)
+  end
 
+  def cart_to_currency(cart)
+    number_to_currency(cart.pack.price * cart.quantity)
+  end
 end
